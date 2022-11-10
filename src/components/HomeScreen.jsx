2446e20697c98972a2d89ea2e5c299d102bc9e96
Manoranjan Sethi <84295391+Manoranjan-Sethi@users.noreply.github.com>
2022-11-10 20:24:23 +0530

    initails

---
+++ src/components/HomeScreen.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../css/HomeScreen.css";
+
+function HomeScreen() {
+  return <div className="homeScreen"></div>;
+}
+
+export default HomeScreen;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCkzRq4xz9PedHvC-qvWlgZhXEU7PTdBWo",
+  authDomain: "netflix-clone-53da6.firebaseapp.com",
+  projectId: "netflix-clone-53da6",
+  storageBucket: "netflix-clone-53da6.appspot.com",
+  messagingSenderId: "878653400183",
+  appId: "1:878653400183:web:647b4ded608585a82f1272",
+};
