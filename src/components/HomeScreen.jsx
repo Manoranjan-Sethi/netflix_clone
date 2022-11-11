f85c7dbb7058ea09546d7e6bf93bbe120a85785b
Manoranjan Sethi <84295391+Manoranjan-Sethi@users.noreply.github.com>
2022-11-11 17:34:59 +0530

    nav

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "../css/HomeScreen.css";
+import Nav from "./Nav";
 
 function HomeScreen() {
-  return <div className="homeScreen"></div>;
+  return (
+    <div className="homeScreen">
+      <Nav />
+      {/* <Banner /> */}
+      {/* <Row /> */}
+    </div>
+  );
 }
 
 export default HomeScreen;
